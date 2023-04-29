94bb30dca5887ee3fee2ead9a0e87972404cf771
Neonchick <57732664+Neonchick@users.noreply.github.com>
2023-04-29 20:50:14 +0200

    fix comments

--- app/src/main/java/com/photoworld/presenter/createprofile/image/CreateProfileImageScreen.kt
+++ app/src/main/java/com/photoworld/presenter/createprofile/image/CreateProfileImageScreen.kt
@@ -5,28 +5,21 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
-import com.bumptech.glide.integration.compose.GlideImage
 import com.photoworld.R
 import com.photoworld.presenter.component.button.BaseButton
+import com.photoworld.presenter.component.grid.TwoColumnsVerticalImageGrid
 import com.photoworld.presenter.component.topbar.TopBar
 import com.photoworld.presenter.theme.Gray500
 
@@ -66,24 +59,7 @@ fun CreateProfileImageScreen(
                         galleryLauncher.launch("image/*")
                     },
                 )
-                LazyVerticalGrid(
-                    columns = GridCells.Fixed(count = 2),
-                    modifier = Modifier
-                        .padding(vertical = 20.dp),
-                ) {
-                    items(viewModel.imageState) { uri ->
-                        GlideImage(
-                            model = uri,
-                            contentDescription = null,
-                            contentScale = ContentScale.Crop,
-                            modifier = Modifier
-                                .padding(4.dp)
-                                .clip(RoundedCornerShape(8.dp))
-                                .aspectRatio(1f)
-                                .fillMaxSize()
-                        )
-                    }
-                }
+                TwoColumnsVerticalImageGrid(uris = viewModel.imageState)
             }
         },
         bottomBar = {
